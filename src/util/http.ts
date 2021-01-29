21a539851b15a6fe3b295e78242b217b5a494929
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-01-29 11:36:07 +0800

    remove showing error component

--- src/util/http.ts
+++ src/util/http.ts
@@ -9,12 +9,12 @@ axios.defaults.withCredentials = true
 
 axios.interceptors.response.use(response => response, err => {
   // 在这里统一处理异常
-  notification.error({
-    message: `请求失败（${err?.response?.status || '无连接'}）`,
-    description: err?.response?.data?.message || err?.response?.data?.msg || err?.response?.data || '服务器未返回数据',
-    class: 'error',
-    duration: 3
-  })
+  // notification.error({
+  //   message: `请求失败（${err?.response?.status || '无连接'}）`,
+  //   description: err?.response?.data?.message || err?.response?.data?.msg || err?.response?.data || '服务器未返回数据',
+  //   class: 'error',
+  //   duration: 3
+  // })
 
   // 将异常继续交给路由层处理
   return Promise.reject(err?.response?.data || { msg: '请求失败' })
